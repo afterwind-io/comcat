0922b29da07609eba2410f062b2a4150414d5615
afterwind-io <spmushroom@hotmail.com>
2021-04-03 15:35:39 +0800

    :recycle: Some housekeeping works

--- dev/index.js
+++ dev/index.js
@@ -5,39 +5,29 @@ import { Comcat, ComcatPump, ComcatPipe } from './comcat/index.js';
 Comcat.enableDebug(true);
 // Comcat.setMode("direct");
 
-class MockPollingPump extends ComcatPump {
-  interval = 10000;
-  intervalHandler = -1;
-
-  connect() {
-    this.intervalHandler = setInterval(() => {
-      this.pump('Kitten', 'such cute');
-      this.pump('Doge', 'much wow');
-    }, this.interval);
-  }
-
-  disconnect() {
-    clearInterval(this.intervalHandler);
-  }
-}
-
-class MockPipe extends ComcatPipe {
-  onMessage(topic, data) {
-    if (window.document.visibilityState === 'hidden') {
-      return;
-    }
-
-    console.log('[mock pipe]', topic, data);
-  }
-}
-
-const pump = new MockPollingPump({
+let intervalHandler = -1;
+const pump = new ComcatPump({
   category: 'mock',
-  mode: 'unique',
 });
+pump.onConnect = () => {
+  intervalHandler = setInterval(() => {
+    pump.pump('Kitten', 'such cute');
+    pump.pump('Doge', 'much wow');
+  }, 10000);
+};
+pump.onDisconnect = () => {
+  clearInterval(intervalHandler);
+};
 pump.start();
 
-const pipe = new MockPipe({
+const pipe = new ComcatPipe({
   topic: 'Doge',
 });
+pipe.onMessage = (topic, data) => {
+  if (window.document.visibilityState === 'hidden') {
+    return;
+  }
+
+  console.log('[mock pipe]', topic, data);
+};
 pipe.start();
