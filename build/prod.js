96e4e60a54f07943fc30fa7462ca9d2addf6a8ad
afterwind-io <spmushroom@hotmail.com>
2021-04-09 11:08:36 +0800

    :wrench: Change build target in production to es5; Split es and umd format

--- build/prod.js
+++ build/prod.js
@@ -8,9 +8,17 @@ const webWorkerLoader = require(path.resolve(
 
 export default {
   input: 'src/index.ts',
-  output: {
-    file: 'dist/comcat.js',
-  },
+  output: [
+    {
+      file: 'dist/comcat.es.js',
+      format: 'es',
+    },
+    {
+      file: 'dist/comcat.js',
+      format: 'umd',
+      name: 'Comcat',
+    },
+  ],
   plugins: [
     webWorkerLoader({
       targetPlatform: 'browser',
@@ -20,6 +28,7 @@ export default {
       tsconfig: 'src/tsconfig.json',
       tsconfigOverride: {
         compilerOptions: {
+          target: 'es5',
           declaration: false,
           removeComments: true,
         },
